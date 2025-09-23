f32eabdcb02529b1798d35a6fbde8dafd748a2d4
Vinicios <contato.viniciosss@gmail.com>
2025-09-23 20:17:05 -0300

    t

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/Pomodoro-online/',
   plugins: [react()],
+  base: '/Pomodoro-online/',
   server: {
     open: true,
     port: 3000,
