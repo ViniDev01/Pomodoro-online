84be808af4deb504348261a1a044788d54c505f1
Vinicios <contato.viniciosss@gmail.com>
2025-09-23 23:39:20 -0300

    url

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import PoliticaDePrivacidade from './PoliticaDePrivacidade.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/Pomodoro-online" element={<App />} />
-      <Route path="/Pomodoro-online/politica-de-privacidade" element={<PoliticaDePrivacidade />} />
+      <Route path="/" element={<App />} />
+      <Route path="/politica-de-privacidade" element={<PoliticaDePrivacidade />} />
     </Routes>
   </BrowserRouter>,
 )
