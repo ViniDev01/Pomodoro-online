0ff50dd79b9db4e5610619116aa3c030adaf34c0
Vinicios <contato.viniciosss@gmail.com>
2025-09-23 23:41:39 -0300

    url2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <footer>
             <span>Feito por Vinicius Santos</span>
-            <Link to="/Pomodoro-online/politica-de-privacidade" className="underline" target="_blank" rel="noopener noreferrer">Politica De Privacidade</Link>
+            <Link to="/politica-de-privacidade" className="underline" target="_blank" rel="noopener noreferrer">Politica De Privacidade</Link>
         </footer>
     )
 }
